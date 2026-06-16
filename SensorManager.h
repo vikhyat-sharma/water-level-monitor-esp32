e5566d0543692170ad04d621c373239faa1d3bf3
Vikhyat Sharma <vikhyats93@gmail.com>
2026-06-16 14:54:15 +0530

    chore: defining new sensor functions

--- SensorManager.h
+++ SensorManager.h
@@ -7,6 +7,10 @@ class SensorManager {
 public:
   void setupSensors();
   float readTankLevel(int trigPin, int echoPin, float tankHeight, float offset);
+  float readTankLevelAverage(int trigPin, int echoPin, float tankHeight, float offset, int samples = 3);
+
+private:
+  float readDistance(int trigPin, int echoPin);
 };
 
 extern SensorManager sensorManager;
