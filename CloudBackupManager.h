89c99460b3281e156bcacef6a2a6a25e96bbe120
Vikhyat Sharma <vikhyats93@gmail.com>
2026-06-22 18:53:37 +0530

    chore: cloud backup implementation header

--- CloudBackupManager.h
+++ CloudBackupManager.h
@@ -2,10 +2,14 @@
 
 #include <Arduino.h>
 #include "Config.h"
+#include <FS.h>
+
+// Cloud backup implementation using HTTPS PUT to a pre-signed Google Cloud Storage URL.
 
 class CloudBackupManager {
 public:
     void begin();
     bool backupNow();
     bool restoreLast();
+    bool uploadFile(const char *spiffsPath, const char *signedUrl);
 };
